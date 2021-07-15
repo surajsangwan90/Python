72474311349c6928db42d081137da17c0a8f79cf
Pratiksha Rajput <pratiksha.rajput@kenbox.in>
2021-07-15 18:16:18 +0530

    array

---
+++ ArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class ArrayList
+{
+public static void main(String[] args)
+{
+Arraylist<String>al = new ArrayList<>();
+al.add("jan");
+al.add("feb");
+al.add("mar");
+System.out.println("al");
+}
+}
---
+++ LinkedList.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+class LinkedListdemo
+{ 
+public static void main(String[] args)
+{
+LinkedList<String>str = new LinkedList<String>;
+list.add("ram");
+list.add("sam");
+list.add("web");
+list.addfirst("pan");
+list.addlast("dem");
+list.add(2,"man");
+Itrator<String>itrator = list.itrator();
+while(itrator.hasnext())
+{
+System.out.println(itrator.next());
+}
+}
+}
+
+
+ 
